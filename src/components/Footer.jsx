d565152214ed8744c1c95c33d31ab11ab24d044d
Prashant Kumar <91178547+sharma4093@users.noreply.github.com>
2024-07-29 16:48:53 +0530

    created cart

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>A Quailty Product By Laxmikant Saraswat 😎
+        <a style={{color:'white', marginLeft:30 }} target="_blank" href="https://laxmikant.co/">Visit Profile!!</a>
+         </p>
+        
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
