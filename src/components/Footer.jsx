da0b643d848ffda7816695f3d1ec0ab79c436164
Prashant Kumar <91178547+sharma4093@users.noreply.github.com>
2024-08-12 13:26:37 +0530

    resovle build error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
   return (
     <>
       <footer>
-        <p>A Quailty Product By Laxmikant Saraswat 😎
-        <a style={{color:'white', marginLeft:30 }} target="_blank" href="https://laxmikant.co/">Visit Profile!!</a>
+        <p>A Quailty Product By prashant sharma
+       
          </p>
         
       </footer>
